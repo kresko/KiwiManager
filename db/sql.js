d719b14c533cad8e9873357121b365668ca07f09
kresko <kresko1995@gmail.com>
2024-11-25 07:43:01 +0100

    [KM-4] - Added assets for categories and products.

--- db/sql.js
+++ db/sql.js
@@ -1,5 +1,5 @@
 const getAllCategoriesSql = `
-    SELECT c.id_category, c.name, i.id_image, i.url, i.type
+    SELECT c.id_category, c.name, c.category_key, i.id_image, i.url, i.type
     FROM categories c
         INNER JOIN images i ON i.id_image = c.fk_image;
 `;
